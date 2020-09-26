b45c2cc1cc733aa464f11c42e3486966936d853e
imran <imufun2@gmail.com>
2020-09-26 20:12:26 +0600

    type of combine

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -15,7 +15,7 @@ example(of: "publisher"){
         .sink { _ in
             print("Notification recived from a publisher!")
         }
-    
+    subscription.cancel()
     
     let center =  NotificationCenter.default
     let observer = center.addObserver(forName: notification, object: nil, queue: nil) { notification in
@@ -24,8 +24,58 @@ example(of: "publisher"){
     center.post(name: notification, object: nil)
     center.removeObserver(observer)
     
-//    print(publisher)
+    //    print(publisher)
 }
+
+
+
+
+// 2
+
+example(of:"Just"){
+    // 1
+    let just = Just("Hellow world")
+    
+    //2
+    _ = just
+        .sink(
+            receiveCompletion: {
+                print("Recived completion: ", $0)
+            },
+            receiveValue: {
+                print("Recived Value: ", $0)
+            })
+}
+
+
+
+// KVO: ( Key value observing )
+
+example(of: "assign(to:on:)"){
+
+ 
+    //1
+    class SomeObject  {
+        var value : String = "" {
+            didSet{
+                print(value)
+            }
+        }
+    }
+    //2
+    
+    let object = SomeObject()
+    
+    let publisher = ["Hello", "World!", "Bangladesh", "iOS", "Android"].publisher
+    
+    _ = publisher.assign(to: \.value, on: object)
+    
+}
+
+
+
+
+
 
 
 public func example(of description: String, action:()-> Void) {
